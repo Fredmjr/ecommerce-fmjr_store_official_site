f3f1e8c59d9b5b2ebf0f2e61275ca529bbe21e84
Fredmjr <fredmjr@gmail.com>
2026-06-08 18:19:33 +0200

    newsletter ui

---
+++ public/src/js/app.uiux.js
@@ -0,0 +1,30 @@
+//reusable element
+const getelem = (e) => {
+  return document.getElementById(e) || document.querySelector(`.${name}`);
+};
+
+//coupon & meail text swap
+let showing = true;
+const newsletter_info1 = "Enter email address";
+const newsletter_info2 = "Get coupons, offers, and more";
+
+const swap_fade_anima = (el, type, cntnt) => {
+  el.style.transition = "opacity 500ms ease-in-out";
+  el.style.opacity = 0;
+  setTimeout(() => {
+    el[type] = cntnt;
+    el.style.opacity = 1;
+  }, 500);
+};
+//newsletter coupon & email placeholder
+(() => {
+  const a = getelem("newsletterinput");
+  setInterval(() => {
+    if (showing) {
+      swap_fade_anima(a, "placeholder", newsletter_info1);
+    } else {
+      swap_fade_anima(a, "placeholder", newsletter_info2);
+    }
+    showing = !showing;
+  }, 10000);
+})();
